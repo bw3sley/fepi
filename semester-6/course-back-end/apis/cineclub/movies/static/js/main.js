4f72fbbb3598962b815f90f5c091fdfaa822614f
Wesley Bernardes <bw3sley@gmail.com>
2024-10-18 00:27:01 -0300

    :art: styles: add design improvements

--- semester-6/course-back-end/apis/cineclub/movies/static/js/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.movie a').forEach(movieLink => {
-    movieLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.location.href = this.href;
-    });
-});

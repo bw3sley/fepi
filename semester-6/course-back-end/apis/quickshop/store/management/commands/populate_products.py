5bbe371541ee3d749b521fb1ff317e6e559ed234
bw3sley <bw3sley@gmail.com>
2024-10-28 18:38:09 -0300

    :tada: chore: initial commit

---
+++ semester-6/course-back-end/apis/quickshop/store/management/commands/populate_products.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from store.models import Product
+from faker import Faker
+import random
+
+class Command(BaseCommand):
+    help = 'Popula o banco de dados com produtos fictícios'
+
+    def handle(self, *args, **kwargs):
+        fake = Faker()
+        for _ in range(20):
+            Product.objects.create(
+                name=fake.word().capitalize(),
+                description=fake.text(),
+                price=round(random.uniform(10, 100), 2),
+                discount=round(random.uniform(5, 50), 2) if random.choice([True, False]) else None,
+                image='products/default.jpg',
+            )
+        self.stdout.write(self.style.SUCCESS('Produtos fictícios criados com sucesso!'))
---
+++ semester-6/course-back-end/apis/quickshop/store/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+def add_to_cart(request, pk):
+    cart = request.session.get('cart', [])
+    if pk not in cart:
+        cart.append(pk)
+        request.session['cart'] = cart
+    return redirect('product_list')
+
+def remove_from_cart(request, pk):
+    cart = request.session.get('cart', [])
+    if pk in cart:
+        cart.remove(pk)
+        request.session['cart'] = cart
+    return redirect('product_list')
+
+def view_cart(request):
+    cart = request.session.get('cart', [])
+    products = Product.objects.filter(pk__in=cart)
+    return render(request, 'store/cart.html', {'products': products})
